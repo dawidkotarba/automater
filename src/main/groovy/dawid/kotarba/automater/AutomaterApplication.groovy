967ea5cc259d1f3e9465e0d8dd4e813ac744c749
dawidkotarba <dawidkotarba>
2021-01-18 22:14:45 +0100

    Initial commit

---
+++ src/main/groovy/dawid/kotarba/automater/AutomaterApplication.groovy
@@ -0,0 +1,13 @@
+package dawid.kotarba.automater
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AutomaterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AutomaterApplication, args)
+	}
+
+}
---
+++ src/test/groovy/dawid/kotarba/automater/AutomaterApplicationTests.groovy
@@ -0,0 +1,13 @@
+package dawid.kotarba.automater
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutomaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
