baef7ca07148cd126b3a9e55dedb414a0aa496f4
dawidkotarba <dawidkotarba>
2021-01-22 00:45:26 +0100

    Add retrieval of mouse location in UI

--- src/main/groovy/dawid/kotarba/automater/View.groovy
+++ src/main/groovy/dawid/kotarba/automater/View.groovy
@@ -1,12 +1,18 @@
 package dawid.kotarba.automater
 
+import com.vaadin.flow.component.AttachEvent
+import com.vaadin.flow.component.DetachEvent
 import com.vaadin.flow.component.Key
+import com.vaadin.flow.component.UI
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.html.Label
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.component.page.Push
 import com.vaadin.flow.component.textfield.TextArea
 import com.vaadin.flow.router.Route
+import dawid.kotarba.automater.device.Mouse
 import dawid.kotarba.automater.executor.Plan
 import dawid.kotarba.automater.executor.PlanExecutor
 import dawid.kotarba.automater.executor.PlanParser
@@ -16,10 +22,12 @@ import org.springframework.core.io.ClassPathResource
 import java.util.stream.Collectors
 
 @Route
+@Push
 class View extends VerticalLayout {
 
-    private PlanExecutor executor
-    private PlanParser parser
+    private final PlanExecutor executor
+    private final PlanParser parser
+    private MouseLocationThread thread
 
     @Autowired
     View(PlanExecutor executor) {
@@ -27,6 +35,8 @@ class View extends VerticalLayout {
         this.parser = new PlanParser()
 
         VerticalLayout layout = new VerticalLayout()
+        Label label = new Label()
+        label.text = 'Ala ma kota'
         TextArea textArea = new TextArea()
         def testPlan = new ClassPathResource('plans/TestPlan.txt')
         def testPlanText = testPlan.getFile().readLines().stream().collect(Collectors.joining('\n'))
@@ -43,9 +53,48 @@ class View extends VerticalLayout {
                 new H1("Automater"),
                 layout,
                 new HorizontalLayout(
+                        label,
                         textArea,
                         addButton
                 )
         )
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        def mouseCoords = new Label("Retrieving mouse coordinates...")
+        add(mouseCoords)
+
+        // Start the data feed thread
+        thread = new MouseLocationThread(attachEvent.getUI(), mouseCoords)
+        thread.start()
+    }
+
+    @Override
+    protected void onDetach(DetachEvent detachEvent) {
+        // Cleanup
+        thread.interrupt()
+        thread = null
+    }
+
+    private static class MouseLocationThread extends Thread {
+        private final UI ui
+        private final Label mouseCoords
+
+        MouseLocationThread(UI ui, Label mouseCoords) {
+            this.ui = ui
+            this.mouseCoords = mouseCoords
+        }
+
+        @Override
+        void run() {
+            def mouse = Beans.mouse
+            while (true) {
+                sleep(50)
+                ui.access {
+                    mouseCoords.text = "Mouse coordinates [ X: ${mouse.x}, Y: ${mouse.y} ]"
+                }
+            }
+        }
     }
 }
