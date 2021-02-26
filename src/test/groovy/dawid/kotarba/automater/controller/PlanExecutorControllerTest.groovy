c6df9a234f489ff5e38e821570371513c510aa9d
dawidkotarba <dawidkotarba>
2021-02-26 20:24:21 +0100

    Execute plan by using REST calls - add test

--- src/test/groovy/dawid/kotarba/automater/controller/PlanExecutorControllerTest.groovy
+++ src/test/groovy/dawid/kotarba/automater/controller/PlanExecutorControllerTest.groovy
@@ -1,6 +1,8 @@
 package dawid.kotarba.automater.controller
 
 import dawid.kotarba.automater.device.Mouse
+import dawid.kotarba.automater.executor.Plan
+import dawid.kotarba.automater.executor.PlanExecutor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -20,6 +22,9 @@ class PlanExecutorControllerTest extends Specification {
 
     @Autowired
     private Mouse mouse
+
+    @Autowired
+    private PlanExecutor planExecutor
 
     def 'Should execute a plan by a REST call'() {
         given:
@@ -46,5 +51,30 @@ class PlanExecutorControllerTest extends Specification {
         and:
         mouse.x == 300
         mouse.y == 300
+    }
+
+    def 'Should stop a plan by a REST call'() {
+        given:
+        def plan = new Plan()
+        plan.addExecutionLine('SLEEP of 20000')
+
+        new Thread({
+            planExecutor.start(plan)
+        }).start()
+
+        while (!planExecutor.isStarted()) {
+            sleep(100)
+        }
+
+        assert planExecutor.isStarted()
+
+        when:
+        mockMvc.perform(post("/stop")
+                .contentType(APPLICATION_JSON)
+                .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+
+        then:
+        !planExecutor.isStarted()
     }
 }
