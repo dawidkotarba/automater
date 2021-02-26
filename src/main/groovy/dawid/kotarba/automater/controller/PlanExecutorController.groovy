f518ad861d2df337aeceb4d171f5f650f18c8cd9
dawidkotarba <dawidkotarba>
2021-02-26 20:47:51 +0100

    Throw ResponseStatusException when another plan is in progress - REST calls

--- src/main/groovy/dawid/kotarba/automater/controller/PlanExecutorController.groovy
+++ src/main/groovy/dawid/kotarba/automater/controller/PlanExecutorController.groovy
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 /**
  * This is an alternative way to execute a plan. It can be executed either in UI or by REST calls.
@@ -27,7 +28,7 @@ class PlanExecutorController {
     @PostMapping(path = '/start', consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<PlanStatistics> start(@RequestBody PlanDto dto) {
         if (planExecutor.isStarted()) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, 'Another plan execution is in progress')
         }
         def plan = new Plan(dto.executionLines, dto.sleepBetweenSteps)
         def planStatistics = planExecutor.start(plan)
